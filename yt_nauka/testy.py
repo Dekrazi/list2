4bd61726214a825fbbf4bb2f2b833b812d5478af
Dekrazi <radoslaw.ziajka@gmail.com>
2023-01-26 15:31:31 +0100

    ok

--- yt_nauka/testy.py
+++
@@ -1,8 +0,0 @@
-x = (input("Ania, ile zjadlas kabanosow? duzo/malo? "))
-
-if x.lower() == "duzo":
-    print("\n""Ania Swinka!")
-elif x.lower() == "malo":        
-    print("\n""Ania klamczuszek!")
-else:
-    print("Niezla kupa")    
